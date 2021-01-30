54478478ca47f22c6dafa0b12979b4b3fcce58cc
Sergey <serega.t1986n@gmail.com>
2021-01-30 20:42:15 +0400

    book

--- home.py
+++ home.py
@@ -1,8 +1,3 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.maximize_window()
--- shop.py
+++ shop.py
@@ -5,7 +5,6 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import keyword
 from selenium.webdriver.support.select import Select
 driver = webdriver.Chrome()
 driver.maximize_window()
